1b613b561d872ad840a95313589e382649488b11
Rohit1173 <lcs2020048@iiitl.ac.in>
2022-07-07 18:09:18 +0530

    server

--- server/app1.js
+++ server/app1.js
@@ -12,37 +12,56 @@ console.log('Server is running');
 const io = socket(server);
 
 
+
 const users = {}
+const rooms={}
 
 //Socket.io Connection------------------
 io.on('connection', (socket) => {
     
     console.log("New socket connection: " + socket.id)
-    var username =''
-    var roomname=''    
+    // var username =''
+    // var roomname=''    
     socket.on('user',(userdata)=>{
      const data =JSON.parse(userdata);
-     username =data.name
-     users[socket.id]=username
-      roomname =data.room
+    // username =data.name
+     users[socket.id]=data.name
+    // roomname =data.room
+     rooms[socket.id]=data.room
      console.log(data.name)
-     socket.join(`${roomname}`)
-     console.log(`${username} has joined the room : ${roomname}`)
-    io.to(`${roomname}`).emit('user',JSON.stringify(username));
-    io.to(`${roomname}`).emit('connection',users[socket.id]+" has joined the chat")
+     socket.join(`${rooms[socket.id]}`)
+     console.log(`${users[socket.id]} has joined the room : ${rooms[socket.id]}`)
+    io.to(`${rooms[socket.id]}`).emit('user',JSON.stringify(users[socket.id]));
+    io.to(`${rooms[socket.id]}`).emit('connection',users[socket.id]+" has joined the chat")
     })
         socket.on('chat message', (msg)  => {
-           // io.emit('chat message', users[socket.id]+" : "+msg);
-          const mydata = JSON.parse(msg);
+          const mydata = JSON.parse(msg)
           const chat = mydata.chat
 
           const send = {
             username:users[socket.id],
             chat:chat
           }
-          socket.broadcast.to(`${roomname}`).emit('chat message', JSON.stringify(send))
-          console.log('message: '+roomname+"-"+ users[socket.id]+" : "+chat);
-        });
+          socket.broadcast.to(`${rooms[socket.id]}`).emit('chat message', JSON.stringify(send))
+          console.log('FROM : '+users[socket.id]+'-'+rooms[socket.id]+' : '+chat)
+        })
+        socket.on('chat image',(img)=>{
+          const img_data = Json.parse(img)
+          const my_img = img_data.chat
+          const send ={
+            username:users[socket.id],
+            chat:my_img
+          }
+          socket.broadcast.to(`${rooms[socket.id]}`).emit('chat image',Json.stringify(send))
+          console.log('Image URL: '+rooms[socket.id]+"-"+ users[socket.id]+" : "+chat)
+        })
+
+        socket.on('dis',() => {
+          socket.broadcast.to(`${rooms[socket.id]}`).emit('dis',users[socket.id])
+          console.log(users[socket.id]+" disconnected")
+          socket.disconnect(true)
+          //delete users[socket.id]
+        })
       
     
 })
